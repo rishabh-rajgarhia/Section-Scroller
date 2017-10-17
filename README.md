3acd78632f3e81da47cf407c4cbd2d1046b77fba
Rishabh Rajgarhia <rishabh.rajgarhia@gmail.com>
2017-10-17 23:37:38 +0530

    Project Setup

---
+++ README.md
@@ -0,0 +1,91 @@
+# Section Scroller
+
+A jQuery plugin for smooth scrolling to next section on click of a button!
+
+### [Demo](https://rishabh0112.github.io/quick-start/)
+
+
+## Usage
+
+#### Include
+
+```html
+<head>
+  <!-- Include CSS for section scroller button -->
+  
+  <!-- This following line is optional. Required when using scrollType effects other than "linear" or "swing". -->
+  <script src="jquery.easings.min.js"></script>
+  
+  <script src="sectionscroller.min.js"></script>
+</head>
+```
+`Tip:` For section scroller button styling, you can use the _scrollerButtonSampleCSS.css_ provided or you can use custom style as per your requirements.
+
+
+#### HTML
+```html
+<div class="section-scroll">Content...</div>
+<div class="section-scroll">Content...</div>
+<div class="section-scroll">Content...</div>
+...
+<div id="section-scroller-button">
+    <i class="caret"></i>
+</div>
+```
+`Tip:` For section scroller button, you can use `carret` class (CSS in _scrollerButtonSampleCSS.css_). You can also easily replace it with icons from [Font Awesome](http://fontawesome.io/icons/) or any other vendor.
+
+
+#### jQuery
+```javascript
+$(document).ready(function () {
+    $(".section-scroll").sectionScroller({
+        // Options
+        scrollerButton: "#section-scroller-button"
+    });
+});
+```
+
+## Options
+
+This is the default configuration:
+
+```javascript
+$(".section-scroll").sectionScroller({
+    scrollerButton: "#section-scroller-button",
+    scrollerButtonRotateClass: "rotate",
+    scrollType: "swing",
+    scrollDuration: 1000,
+    offset: 0,
+    fixedNavbar: null,
+    onScrollStart: function () {},
+    onScrollEnd: function () {}
+});
+```
+`".section-scroll"` is the CSS selector for sections of the page.
+
+| Options                   | Default                    | Description  |
+| ------------------------- |----------------------------|--------------|
+| scrollerButton            | "#section-scroller-button" | CSS selector for element on clicking of which, scrolling to the next section will happen. |
+| scrollerButtonRotateClass | "rotate"                   | Class added to the scroller button(to rotate the scroller button direction, etc.) when the last section or the bottom of page is reached. |
+| scrollType                | "swing"                    | Animation / Easing while section scrolling. Can easily be extended using [jQuery Easing Plugin](http://gsgd.co.uk/sandbox/jquery/easing/). |
+| scrollDuration            | 1000                       |  Duration of the section scroll. |
+| offset                    | 0                          |  Offset (pixels) from the top for all sections, e.g., `offset: 10`. For setting offset for individual sections, see below. |
+| fixedNavbar               | null                       |  CSS Selector for fixed top navbar if any. Useful in responsive websites where height of fixed navbar changes in different screen sizes, e.g., `fixedNavbar: ".navbar-fixed-top"`.  |
+| onScrollStart             | Callback Function          |  Callback function fired on start of the section scrollling. |
+| onScrollEnd               | Callback Function          |  Callback function fired on start of the section scrollling. |
+
+For setting offset for individual sections, use `data-scroll-offset` HTML Data Attribute as shown below:
+
+```html
+<div class="section-scroll" data-scroll-offset="30">Content...</div>
+```
+**Extra Options Available Inside Callbacks**
+
+`this.scrollToElementIndex` (Number) - index of the destination section - starting with 0 by default.  
+`this.elementOffsetTops` (Array) - offet top of all the sections.  
+`this.dataScrollOffsets` (Array) - offset provided in the HTML Data Attribute(data-scroll-offset) if any.  
+
+
+## License
+
+This project is licensed under the MIT License (http://opensource.org/licenses/MIT).
---
+++ sectionScroller.js
@@ -0,0 +1,183 @@
+/*
+ * Section Scroller
+ * 
+ * Licensed under the MIT license
+ *
+ * Copyright (c) 2017 Rishabh Rajgarhia | https://github.com/rishabh-rajgarhia
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+;(function ($, window, document) {
+	"use strict";
+
+	var $window = $(window);
+	var $document = $(document);
+
+	// default options
+	var defaults = {
+		scrollerButton: "#section-scroller-button",
+		scrollerButtonRotateClass: "rotate",
+		scrollType: "swing",
+		scrollDuration: 1000,
+		offset: 0,
+		fixedNavbar: null,
+
+		// callbacks
+		onScrollStart: function () {},
+		onScrollEnd: function () {}
+	};
+
+	var SectionScroller = function (elements, options) {
+		this.elements = elements;
+
+		// options
+		this.options = $.extend({}, defaults, options);
+
+		// scroller button
+		this.scrollerButton = $(this.options.scrollerButton);
+		// class added to the scroller button(to rotate the scroller button direction, etc.) when the last section or the bottom of page is reached
+		this.scrollerButtonRotateClass = this.options.scrollerButtonRotateClass;
+
+		// animation duration in milliseconds
+		this.scrollDuration = this.options.scrollDuration;
+
+		// animation type - swing or linear. To extend use jquery easing
+		this.scrollType = this.options.scrollType;
+
+		// custom offset top
+		this.offset = this.options.offset;
+
+		// in case of fixed navbar - useful in responsive websites where height of fixed navbar changes in different screen sizes
+		this.fixedNavbar = this.options.fixedNavbar;
+
+		// index of the destination element - starting with 0 by default
+		this.scrollToElementIndex = 0;
+
+		// offet top of all the elements
+		this.elementOffsetTops = [];
+
+		// offset provided in the data attribute(data-scroll-offset) if any
+		this.dataScrollOffsets = [];
+
+		this._defaults = defaults;
+
+		this.init();
+	}
+
+	SectionScroller.prototype = {
+
+		constructor: SectionScroller,
+
+		init: function () {
+			var that = this;
+
+			this.calculateElementOffets(this);
+			$window.resize(function () {
+				that.calculateElementOffets(that)
+			});
+
+			this.scrollerButton.on("click", function (event) {
+				that.scrollToNextElement(that);
+				event.preventDefault();
+			});
+
+			this.scrollerButtonRotateClassHandler(this);
+			$window.scroll(function () {
+				that.scrollerButtonRotateClassHandler(that)
+			});
+		},
+
+		calculateElementOffets: function (that) {
+			that.elementOffsetTops = [];
+			that.dataScrollOffsets = [];
+
+			that.elements.each(function () {
+				that.elementOffsetTops.push($(this).offset().top);
+				that.dataScrollOffsets.push(Number($(this).data("scroll-offset")) ? Number($(this).data("scroll-offset")) : 0);
+			});
+
+		},
+
+		scrollToNextElement: function (that) {
+			var index = 0;
+			var offsetAdjustment = that.offset + getFixedNavbarHeight(that.fixedNavbar);
+
+			while (parseInt($window.scrollTop() + 2) >= parseInt(that.elementOffsetTops[index] - offsetAdjustment) && index < that.elementOffsetTops.length)
+				index++;
+
+			if (index >= that.elementOffsetTops.length || checkBottom())
+				index = 0;
+
+			that.scrollToElementIndex = index;
+
+			// callback function on start of the scrollling
+			that.options.onScrollStart.call(that);
+
+			$('html, body').stop().animate({
+				scrollTop: parseInt(that.elementOffsetTops[index] - offsetAdjustment) + parseInt(that.dataScrollOffsets[index])
+			}, that.scrollDuration, that.scrollType).promise().done(function () {
+				that.options.onScrollEnd.call(that); // callback function on end of the scrolling
+			});
+
+		},
+
+		scrollerButtonRotateClassHandler: function (that) {
+			var offsetAdjustment = that.offset + getFixedNavbarHeight(that.fixedNavbar);
+			var rotateClassOffset = parseInt($(that.elements[that.elements.length - 1]).outerHeight() / 3);
+
+			if (checkBottom())
+				addClass(that.scrollerButton, that.scrollerButtonRotateClass);
+			else if (parseInt($window.scrollTop()) >= parseInt(that.elementOffsetTops[that.elementOffsetTops.length - 1] - offsetAdjustment - rotateClassOffset))
+				addClass(that.scrollerButton, that.scrollerButtonRotateClass);
+			else
+				removeClass(that.scrollerButton, that.scrollerButtonRotateClass);
+		}
+	};
+
+	// check if the page has reached the bottom
+	function checkBottom() {
+		return parseInt($window.scrollTop() + $window.height()) === parseInt($document.height()) ? true : false;
+	}
+
+	function getFixedNavbarHeight(fixedNavbar) {
+		return fixedNavbar === null ? 0 : typeof fixedNavbar === "string" && $(fixedNavbar).length > 0 ? $(fixedNavbar).outerHeight() : console.log("Section Scroller Error: Wrong fixedNavbar option provided!");
+	}
+
+	function addClass(element, classToBeAdded) {
+		if (!element.hasClass(classToBeAdded))
+			element.addClass(classToBeAdded);
+	}
+
+	function removeClass(element, classToBeRemoved) {
+		if (element.hasClass(classToBeRemoved))
+			element.removeClass(classToBeRemoved);
+	}
+
+	$.fn.sectionScroller = function (options) {
+		if (!$.data(this, "plugin_" + "sectionScroller")) {
+			$.data(this, "plugin_" + "sectionScroller",
+				new SectionScroller(this, options));
+		}
+
+		return this;
+	};
+
+})(jQuery, window, document);
